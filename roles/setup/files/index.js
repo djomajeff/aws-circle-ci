674c0c4354cb61b602f99aeab9a18a90a77fa396
djomajeff <djomajeff@gmail.com>
2023-02-12 20:41:17 +0100

    Final working version

--- roles/setup/files/index.js
+++ roles/setup/files/index.js
@@ -1,6 +1,6 @@
 const http = require("http");
 const server = http.createServer(function (req, res) {
   res.writeHead(200);
-  res.end("Hello world! from Ansible");
+  res.end("Hello world! from Jeffrey Ansible");
 });
 server.listen(3000);
